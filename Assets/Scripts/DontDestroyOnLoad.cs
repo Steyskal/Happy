e936eb84a75821435ca378cbb6402e227a4188c3
Toni Steyskal <toni.steyskal@gmail.com>
2016-12-19 23:23:16 +0100

    Initial Package

---
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *
+ *	Happy
+ *	by Toni Steyskal, 2016-2017
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Happy
+{
+	[DisallowMultipleComponent]
+	public class DontDestroyOnLoad : MonoBehaviour
+	{
+		void Awake ()
+		{
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/UnityEventExtension.cs
@@ -0,0 +1,28 @@
+﻿/*
+ *
+ *	Happy
+ *	by Toni Steyskal, 2016-2017
+ * 
+ */
+
+using System;
+
+using UnityEngine.Events;
+
+namespace Happy
+{
+	[Serializable]
+	public class CustomUnityEvent : UnityEvent {}
+
+	[Serializable]
+	public class CustomUnityEvent<T0> : UnityEvent<T0> {}
+
+	[Serializable]
+	public class CustomUnityEvent<T0, T1> : UnityEvent<T0, T1> {}
+
+	[Serializable]
+	public class CustomUnityEvent<T0, T1, T2> : UnityEvent<T0, T1, T2> {}
+
+	[Serializable]
+	public class CustomUnityEvent<T0, T1, T2, T3> : UnityEvent<T0, T1, T2, T3> {}
+}

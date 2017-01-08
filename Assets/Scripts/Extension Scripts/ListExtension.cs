6df62b9b71a2a39d61b1d12a1c29966c3dcea370
Toni Steyskal <toni.steyskal@gmail.com>
2017-01-08 15:06:32 +0100

    ListExtension added

---
+++ Assets/Scripts/Extension Scripts/ListExtension.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *
+ *	Happy
+ *	by Toni Steyskal, 2016-2017
+ * 
+ */
+
+using System.Collections.Generic;
+
+namespace Happy
+{
+	public static class ListExtension
+	{
+		public static void Swap<T> (this List<T> list, int indexA, int indexB)
+		{
+			T tmp = list [indexA];
+			list [indexA] = list [indexB];
+			list [indexB] = tmp;
+		}
+	}
+}

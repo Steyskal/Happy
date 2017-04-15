02dc3f4da3f06711fa4a39461a8a67e18d430e98
Toni Steyskal <toni.steyskal@gmail.com>
2017-04-15 07:48:00 +0200

    ColorExtension added

---
+++ Assets/Scripts/Extension Scripts/ColorExtension.cs
@@ -0,0 +1,37 @@
+﻿/*
+ *
+ *	Happy
+ *	by Toni Steyskal, 2017
+ * 
+ */
+
+using UnityEngine;
+
+namespace Happy
+{
+	public static class ColorExtension
+	{
+		#region Unity Colors
+
+		public static Color Black = Color.black;
+		public static Color Blue = Color.blue;
+		public static Color Clear = Color.clear;
+		public static Color Cyan = Color.cyan;
+		public static Color Gray = Color.gray;
+		public static Color Green = Color.green;
+		public static Color Magenta = Color.magenta;
+		public static Color Red = Color.red;
+		public static Color White = Color.white;
+		public static Color Yellow = Color.yellow;
+
+		#endregion
+
+
+		#region Extension Colors
+
+		public static Color Orange = new Color (1.0f, 0.5f, 0.0f);
+		public static Color Purple = new Color (0.5f, 0.0f, 1.0f);
+
+		#endregion
+	}
+}

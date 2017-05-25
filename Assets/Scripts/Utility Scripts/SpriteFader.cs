883641568626bd3d02ee1f3b896c35053eb71e01
Toni Steyskal <toni.steyskal@gmail.com>
2017-05-25 18:11:56 +0200

    SpriteFader added FPS Display updated Update to Unity v5.6.1f1

---
+++ Assets/Scripts/Utility Scripts/SpriteFader.cs
@@ -0,0 +1,62 @@
+﻿/*
+ *
+ *  Happy
+ *  by Toni Steyskal, 2016-2017
+ * 
+ */
+
+using System.Collections;
+
+using UnityEngine;
+
+namespace Happy
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteFader : MonoBehaviour
+    {
+        public enum FadeType
+        {
+            FadeIn,
+            FadeOut
+        }
+
+        public bool FadeOnAwake = true;
+        public FadeType Type = FadeType.FadeIn;
+        [Positive]
+        public float Duration = 1.0f;
+
+        private SpriteRenderer _spriteRenderer;
+
+        void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+
+            if (FadeOnAwake)
+                StartCoroutine(Fade(Type, Duration));
+        }
+
+        public IEnumerator Fade(FadeType type, float duration)
+        {
+            WaitForFixedUpdate waitForFixedUpdate = new WaitForFixedUpdate();
+
+            Color spriteColor = _spriteRenderer.color;
+            spriteColor.a = type == FadeType.FadeIn ? 0.0f : 1.0f; ;
+            _spriteRenderer.color = spriteColor;
+
+            float requiredAlpha = type == FadeType.FadeIn ? 1.0f : 0.0f;
+            float fadeInterval = (requiredAlpha - _spriteRenderer.color.a) / (duration / Time.fixedDeltaTime);
+
+            bool shouldFade = true;
+
+            while (shouldFade)
+            {
+                spriteColor.a += fadeInterval;
+                _spriteRenderer.color = spriteColor;
+
+                shouldFade = type == FadeType.FadeIn ? _spriteRenderer.color.a < requiredAlpha : _spriteRenderer.color.a > requiredAlpha;
+
+                yield return waitForFixedUpdate;
+            }
+        }
+    }
+}
